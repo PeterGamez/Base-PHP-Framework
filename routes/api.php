aa2f5f339160a7c82c2699980e717186ce32d93f
PeterGamez <chanakan.kea@gmail.com>
2023-12-26 10:15:44 +0700

    add preflight requests

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,12 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json; charset=UTF-8');
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Respond to preflight requests (sent by some browsers before the actual request)
+    http_response_code(200);
+    exit();
+}
+
 $agent_request = array_slice($agent_request, 2);
 
 $version = isset($agent_request[0]) ? $agent_request[0] : null;
