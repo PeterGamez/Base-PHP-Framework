7392d4a7b9a3adc7e5d4ec9559762ff6d9d04473
PeterGamez <chanakan.kea@gmail.com>
2024-03-14 16:05:55 +0700

    fix default setting

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ return [
      * Default Database connection
      */
 
-    "default" => env("DB_CONNECTION", "pgsql"),
+    "default" => env("DB_CONNECTION", "mysql"),
 
     /**
      * Database Connections
@@ -16,7 +16,7 @@ return [
             "driver" => "mysql",
             "host" => env("DB_HOST", "127.0.0.1"),
             "port" => env("DB_PORT", "3306"),
-            "database" => env("DB_DATABASE", "forge"),
+            "database" => env("DB_DATABASE", "framework"),
             "username" => env("DB_USERNAME", "root"),
             "password" => env("DB_PASSWORD", ""),
             "charset" => "utf8mb4",
